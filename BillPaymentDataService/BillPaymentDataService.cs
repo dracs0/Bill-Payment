f1dfd3327cdd8ea81abdf092c75623203a695447
dracs0 <johnrenmar2003@gmail.com>
2026-04-16 17:42:13 +0800

    Initial commit for Bill-Payment project

---
+++ BillPaymentDataService/BillPaymentDataService.cs
@@ -0,0 +1,19 @@
+﻿using BillsLibrary;
+
+namespace BillPaymentDataService
+{
+    public class PaymentDataService
+    {
+        private readonly List<Models> _payments = new List<Models>();
+
+        public void SavePayment(Models payment)
+        {
+            _payments.Add(payment);
+        }
+
+        public IEnumerable<Models> GetPayments()
+        {
+            return _payments;
+        }
+    }
+}
